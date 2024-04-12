9977e9707e13882ec2def659b4e42e281ea56451
Renan Perissé <renanperissedeoliveira@gmail.com>
2024-04-11 21:06:01 -0300

    First commit

---
+++ src/test/java/com/xbrain/vendasapi/VendasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbrain.vendasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
